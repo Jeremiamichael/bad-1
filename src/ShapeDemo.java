1ea63d3f6846b5770c89373b63b0cfee0456211e
Jeremiamichael <jeremia.mikhael@binus.ac.id>
2025-07-19 20:53:51 +0700

    updated

--- src/ShapeDemo.java
+++ src/ShapeDemo.java
@@ -1,9 +1,9 @@
 public class ShapeDemo {
     public static void main(String[] args) {
         Shape[] shapes = new Shape[3];
-        shapes[0] = new Square("Alice", 5.0);
-        shapes[1] = new Triangle("Bob", 4.0, 3.0);
-        shapes[2] = new Circle("Charlie", 2.5);
+        shapes[0] = new Square("Jeremia", 5.0);
+        shapes[1] = new Triangle("Johanes", 4.0, 3.0);
+        shapes[2] = new Circle("Michael", 2.5);
         
         for (Shape shape : shapes) {
             System.out.println("Owner: " + shape.getOwner());
